d551220bf6911362efea282e5dc46af221aec61a
Kyle Engan <kyle.engan@gmail.com>
2017-06-06 16:12:29 -0400

    Added 25C button and test for initial text.  Getting all the layout done first before functionality.

--- app/src/test/java/com/pillartechnology/kengan/mobilevendingmachine/MainActivityTest.java
+++ app/src/test/java/com/pillartechnology/kengan/mobilevendingmachine/MainActivityTest.java
@@ -3,6 +3,7 @@ package com.pillartechnology.kengan.mobilevendingmachine;
 import com.pillartechnology.kengan.mobilevendingmachine.BuildConfig;
 
 import android.app.Activity;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.junit.Before;
@@ -19,15 +20,22 @@ import static org.junit.Assert.assertEquals;
 public class MainActivityTest {
 
     TextView statusScreen;
+    Button quarterButton;
 
     @Before
     public void setUp() {
         Activity activity = Robolectric.setupActivity(MainActivity.class);
         statusScreen = (TextView) activity.findViewById(R.id.statusScreen);
+        quarterButton = (Button) activity.findViewById(R.id.quarterButton);
     }
 
     @Test
     public void statusScreenReadsINSERT_COIN_whenInitialized() {
         assertEquals("INSERT COIN", statusScreen.getText());
+    }
+
+    @Test
+    public void quarterButtonReads25C_whenInitialized() {
+        assertEquals("25C", quarterButton.getText());
     }
 }
